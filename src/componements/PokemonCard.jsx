99466e56ce9b9d9d6080ef4d0569c7ab9e4f9e92
NICOLASR44 <nicolasrinfert@gmail.com>
2024-06-11 10:47:40 +0200

    Add new Pokemon Mew

--- src/componements/PokemonCard.jsx
+++ src/componements/PokemonCard.jsx
@@ -1,15 +1,22 @@
-import React from "react";
-
 const PokemonCard = () => {
+  const pokemon = pokemonList[1]; // Mettre sur 1 pour tester avec "mew"
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
 
 export default PokemonCard;
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
